5679e1f5c873bda33e4d2088a3529ee1b564e58c
Anand Kulkarni <anandkulkarni67@gmail.com>
2026-03-14 14:44:34 -0700

    update scripts

--- aws-resources/application/scripts/deploy.sh
+++ aws-resources/application/scripts/deploy.sh
@@ -2,32 +2,31 @@
 
 application_name=$1
 template_file="./aws-resources/application/resources.yaml"
-# Replace with your actual region if different
 region="us-east-1" 
 
-echo "Starting AWS CloudFormation stack creation for $application_name..."
+echo "Starting AWS CloudFormation stack creation/ updation for $application_name..."
 
-aws cloudformation create-stack \
+aws cloudformation deploy \
     --stack-name "$application_name-application" \
     --template-body file://"$template_file" \
     --region "$region" \
     --capabilities CAPABILITY_AUTO_EXPAND
 
 if [ $? -ne 0 ]; then
-    echo "Failed to initiate stack creation. Exiting."
+    echo "Failed to create/ update stack. Exiting."
     exit 1
 fi
 
-echo "Creation initiated. Waiting for stack to reach CREATE_COMPLETE status..."
+# echo "Creation initiated. Waiting for stack to reach CREATE_COMPLETE status..."
 
-aws cloudformation wait stack-create-complete \
-    --stack-name "$application_name-application" \
-    --region "$region"
+# aws cloudformation wait stack-create-complete \
+#     --stack-name "$application_name-application" \
+#     --region "$region"
 
-if [ $? -eq 0 ]; then
-    echo "Stack $application_name is CREATE_COMPLETE. Proceeding to the next script."
-else
-    echo "Stack $application_name failed to create or the wait command timed out."
-    echo "Check the AWS CloudFormation console or use 'aws cloudformation describe-stack-events' for details."
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "Stack $application_name is CREATE_COMPLETE. Proceeding to the next script."
+# else
+#     echo "Stack $application_name failed to create or the wait command timed out."
+#     echo "Check the AWS CloudFormation console or use 'aws cloudformation describe-stack-events' for details."
+#     exit 1
+# fi
--- aws-resources/github-oidc/scripts/deploy.sh
+++ aws-resources/github-oidc/scripts/deploy.sh
@@ -3,12 +3,11 @@
 application_name=$1
 github_username=$2
 template_file="./aws-resources/github-oidc/resources.yaml"
-# Replace with your actual region if different
 region="us-east-1"
 
-echo "Starting AWS CloudFormation stack creation for $application_name..."
+echo "Starting AWS CloudFormation stack creation/ updation for $application_name..."
 
-aws cloudformation create-stack \
+aws cloudformation deploy \
     --stack-name "$application_name-github-oidc" \
     --template-body file://"$template_file" \
     --region "$region" \
@@ -16,20 +15,20 @@ aws cloudformation create-stack \
     --capabilities CAPABILITY_NAMED_IAM
 
 if [ $? -ne 0 ]; then
-    echo "Failed to initiate stack creation. Exiting."
+    echo "Failed to create/ update stack. Exiting."
     exit 1
 fi
 
-echo "Creation initiated. Waiting for stack to reach CREATE_COMPLETE status..."
+# echo "Creation initiated. Waiting for stack to reach CREATE_COMPLETE status..."
 
-aws cloudformation wait stack-create-complete \
-    --stack-name "$application_name-github-oidc" \
-    --region "$region"
+# aws cloudformation wait stack-create-complete \
+#     --stack-name "$application_name-github-oidc" \
+#     --region "$region"
 
-if [ $? -eq 0 ]; then
-    echo "Stack $application_name is CREATE_COMPLETE. Proceeding to the next script."
-else
-    echo "Stack $application_name failed to create or the wait command timed out."
-    echo "Check the AWS CloudFormation console or use 'aws cloudformation describe-stack-events' for details."
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "Stack $application_name is CREATE_COMPLETE. Proceeding to the next script."
+# else
+#     echo "Stack $application_name failed to create or the wait command timed out."
+#     echo "Check the AWS CloudFormation console or use 'aws cloudformation describe-stack-events' for details."
+#     exit 1
+# fi
