7f9a71faa108d3b1d710b1d55da3c4a9e26b2b2f
Anand Kulkarni <anandkulkarni67@gmail.com>
2026-03-16 11:27:18 -0700

    update

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -8,4 +8,10 @@ cd api
 
 artifact_version=$(npm --loglevel silent run version)
 
+echo "$artifact_version"
+
+echo "$PWD"
+
+echo "$(ls -l)"
+
 aws lambda update-function-code --function-name "$application_name-api-lambda" --zip-file fileb://$artifact_vesion.zip
